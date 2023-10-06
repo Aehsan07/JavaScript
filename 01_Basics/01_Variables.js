0cc5eb730290286d5a50414e49de4af3370fd11b
Aehsan07 <55535160+Aehsan07@users.noreply.github.com>
2023-10-06 12:16:35 +0000

    commitChanges

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345678
+let accountEmail = "aehsan2094@gmail.com"
+var accountPassword="12345"
+accountCity='Vadodara'
+
+//accountId="12345"
+accountEmail="aehsan2294@gmail.com"
+accountPassword="67890"
+accountCity="Jambusar"
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
