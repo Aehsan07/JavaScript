6fe1f13b8caa179f5393a97efaf2313a9e5631d6
Aehsan07 <55535160+Aehsan07@users.noreply.github.com>
2023-10-14 12:12:00 +0000

    DataTypes

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -1,1 +1,21 @@
 "use strict"; // treat all code as newer version of JavaScript
+console.log(3 + 3);
+console.log("Aehsan");
+
+let name = "Aehsan"
+let age = 18
+let isLoggedIn = true
+
+//Number => 2 to power 53
+//bigint => 
+//String => ""
+//Boolen => TRUE or FALSE
+//null => standalone value
+//undefiend 
+//symbol => unique
+
+//object
+
+console.log(typeof "Aehsan");
+console.log(typeof null); // object
+console.log(typeof undefined); //undeifned
